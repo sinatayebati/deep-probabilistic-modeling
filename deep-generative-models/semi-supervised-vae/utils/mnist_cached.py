056de286426fc1aaf47ec1c861ccedc08d65c0bb
sinatayebati <sinatayebati@gmail.com>
2023-08-14 23:57:00 -0500

    updates committed

--- deep-generative-models/semi-supervised-vae/utils/mnist_cached.py
+++ deep-generative-models/semi-supervised-vae/utils/mnist_cached.py
@@ -11,3 +11,18 @@ from pyro.contrib.examples.util import MNIST, get_data_directory
 # this file contains utilities for caching, transforming and splitting MNIST data
 # efficiently. By default, a PyTorch DataLoader will apply the transform every epoch
 # we avoid this by caching the data early on in MNISTCached class
+
+# transfromations for MNIST data
+def fn_x_mnist(x, use_cuda):
+    # normalize pixel values of the image to be in [0,1] instead of [0,255]
+    xp = x * (1.0 / 255)
+
+    # transform x to a linear tensor from bx * a1 * a2 * ... --> bs * A
+    xp_1d_size = reduce(lambda a, b: a * b, xp.size()[1:])
+    xp = xp.view(-1, xp_1d_size)
+
+    # send the data to GPU
+    if use_cuda:
+        xp = xp.cuda()
+
+    return xp
